262b6ea83b5e13407c18d4f968cd03fc4866da3f
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-05 17:19:37 +0200

    add a custom button and use it in our placeItems

--- client/src/places/components/PlaceItem.js
+++ client/src/places/components/PlaceItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './PlaceItem.css';
 import Card from '../../shared/components/UIElements/Card';
-
+import Button from '../../shared/components/FormElements/Button';
 
 const PlaceItem = (props) => {
 	return (
@@ -19,9 +19,9 @@ const PlaceItem = (props) => {
 				</div>
 
 				<div className="place-item__actions">
-					<button>VIEW ON MAP</button>
-					<button>EDIT</button>
-					<button>DELETE</button>
+					<Button inverse>VIEW ON MAP</Button>
+					<Button to={`/places/${props.id}`}>EDIT</Button>
+					<Button>DELETE</Button>
 				</div>
 			</Card>
 		</li>
