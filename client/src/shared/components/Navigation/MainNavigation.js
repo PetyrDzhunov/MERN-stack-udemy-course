a3eed2fa723cb9036858aec6b1ab64063fa4e1f8
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-05 16:37:04 +0200

    add a new third party library for animations and animate our side drawer to show/close with animation

--- client/src/shared/components/Navigation/MainNavigation.js
+++ client/src/shared/components/Navigation/MainNavigation.js
@@ -1,7 +1,6 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import Backdrop from '../UIElements/Backdrop';
 import MainHeader from './MainHeader';
 import './MainNavigation.css';
 import NavLinks from './NavLinks';
@@ -9,15 +8,26 @@ import SideDrawer from './SideDrawer';
 
 
 const MainNavigation = props => {
+	const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+	const openDrawerHandler = () => {
+		setDrawerIsOpen(true);
+	};
+
+	const closeDrawerHandler = () => {
+		setDrawerIsOpen(false);
+	};
+
 	return (
 		<>
-			<SideDrawer>
+			{drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+			<SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
 				<nav className="main-navigation__drawer">
 					<NavLinks />
 				</nav>
 			</SideDrawer>
 			<MainHeader>
-				<button className="main-navigation__menu-btn">
+				<button onClick={openDrawerHandler} className="main-navigation__menu-btn">
 					<span></span>
 					<span></span>
 					<span></span>
@@ -25,7 +35,7 @@ const MainNavigation = props => {
 				<h1 className="main-navigation__title">
 					<Link to="/">YourPlaces</Link>
 				</h1>
-				<nav>
+				<nav className="main-navigation__header-nav">
 					<NavLinks />
 				</nav>
 			</MainHeader>
