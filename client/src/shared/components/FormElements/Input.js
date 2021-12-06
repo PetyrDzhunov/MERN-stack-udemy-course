600739c1420e289e9df000ab938d0cc8baf0bd48
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-06 11:27:10 +0200

    useEffect and useCallback in order to escape infinite loop

--- client/src/shared/components/FormElements/Input.js
+++ client/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import './Input.css';
 
 import { validate } from '../../util/validators';
@@ -27,9 +27,17 @@ const inputReducer = (state, action) => {
 const Input = (props) => {
 	const [inputState, dispatch] = useReducer(inputReducer, { value: '', isTouched: false, isValid: false });
 
+	const { id, onInput } = props;
+	const { value, isValid } = inputState;
+
+	useEffect(() => {
+		onInput(id, value, isValid)
+	}, [id, value, isValid, onInput])
+
+
 	const changeHandler = (event) => {
 		dispatch({ type: 'CHANGE', val: event.target.value, validators: props.validators });
-	}
+	};
 
 	const touchHandler = () => {
 		dispatch({
