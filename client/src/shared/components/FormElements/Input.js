ccb7cb265dac1531d9e2357cd527242136cf5f50
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-06 11:11:59 +0200

    add a validator util function and use it in the Input component which we updated

--- client/src/shared/components/FormElements/Input.js
+++ client/src/shared/components/FormElements/Input.js
@@ -1,25 +1,41 @@
 import React, { useReducer } from 'react'
 import './Input.css';
 
+import { validate } from '../../util/validators';
+
 const inputReducer = (state, action) => {
 	switch (action.type) {
 		case 'CHANGE':
 			return {
 				...state,
 				value: action.val,
-				isValid: true
+				isValid: validate(action.val, action.validators)
 			};
+
+		case "TOUCH":
+			return {
+				...state,
+				isTouched: true
+			}
 		default:
 			return state;
 	};
 };
 
+
+
 const Input = (props) => {
-	const [inputState, dispatch] = useReducer(inputReducer, { value: '', isValid: false });
+	const [inputState, dispatch] = useReducer(inputReducer, { value: '', isTouched: false, isValid: false });
 
 	const changeHandler = (event) => {
-		dispatch({ type: 'CHANGE', val: event.target.value });
+		dispatch({ type: 'CHANGE', val: event.target.value, validators: props.validators });
 	}
+
+	const touchHandler = () => {
+		dispatch({
+			type: "TOUCH"
+		});
+	};
 
 	const element = props.element === 'input' ?
 		<input
@@ -28,21 +44,23 @@ const Input = (props) => {
 			placeholder={props.placeholder}
 			onChange={changeHandler}
 			value={inputState.value}
+			onBlur={touchHandler}
 		/>
 		: <textarea
 			onChange={changeHandler}
 			id={props.id}
 			rows={props.rows || 3}
 			value={inputState.value}
+			onBlur={touchHandler}
 		/>
 
 
 
 	return (
-		<div className={`form-control ${!inputState.isValid && 'form-control--invalid'}`}>
+		<div className={`form-control ${!inputState.isValid && inputState.isTouched && 'form-control--invalid'}`}>
 			<label htmlFor={props.id}>{props.label}</label>
 			{element}
-			{!inputState.isValid && <p>{props.errorText}</p>}
+			{!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
 		</div>
 	);
 };
