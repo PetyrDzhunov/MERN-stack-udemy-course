5c34c44ea0bc99b60302d2639688d24442c677ea
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-07 16:53:55 +0200

    finished our base back-end API connected to our database and we now return data as JSON which can be fetched by the frontend now

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -25,18 +25,20 @@ const getPlaceById = async (req, res, next) => {
 
 const getPlacesByUserId = async (req, res, next) => {
 	const { uid } = req.params;
-	let places;
+	let userWithPlaces;
 	try {
-		places = await Place.find({ creator: uid });
+		userWithPlaces = await User.findById(uid).populate('places');
 	} catch (err) {
+		console.log(err);
 		const error = new HttpError('Fetching places failed, please try again later', 500);
 		return next(error);
 	};
 
-	if (!places || places.length === 0) {
+	if (!userWithPlaces || userWithPlaces.length === 0) {
 		return next(new HttpError('Could not find places for the provided user id,', 404));
 	};
-	res.json({ places: places.map(place => place.toObject({ getters: true })) });
+
+	res.json({ places: userWithPlaces.places.map(place => place.toObject({ getters: true })) });
 };
 
 const createPlace = async (req, res, next) => {
