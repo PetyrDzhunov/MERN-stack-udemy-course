5a6e744cbaa24f77c79fa6141a48225d5026567d
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-08 14:21:19 +0200

    use the custom http-hook also to fetch users on initial load on the home page

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,41 +1,29 @@
 import React, { useEffect, useState } from "react";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
 	const [users, setUsers] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState();
+	const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
 	useEffect(() => {
-		const sendRequest = async () => {
-			setIsLoading(true);
+		const fetchUsers = async () => {
 			try {
-				const response = await fetch('http://localhost:5000/api/users');
-				const responseData = await response.json();
-
-				if (!response.ok) {
-					throw new Error(responseData.message);
-				};
+				const responseData = await sendRequest('http://localhost:5000/api/users');
 
 				setUsers(responseData.users);
-				setIsLoading(false);
 			} catch (err) {
-				setIsLoading(false);
-				setError(err.message);
 			};
 		};
-		sendRequest();
-	}, [])
+		fetchUsers();
+	}, [sendRequest])
 
-	const errorHandler = () => {
-		setError(null);
-	};
 
 	return (
 		<>
-			<ErrorModal error={error} onClear={errorHandler} />
+			<ErrorModal error={error} onClear={clearError} />
 			{isLoading && <div className="center"><LoadingSpinner /></div>}
 			{!isLoading && users && <UsersList items={users} />}
 		</>
