9d708dc519c57e2d76e9d528babd4445cab21982
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-08 13:10:05 +0200

    fetch all our users asynchronously from our back-end and display them when we load them , handle errors and loading states aswell

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
+	const [users, setUsers] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState();
 
-	const USERS = [
-		{ id: "u1", name: "Peter Dzhunov", image: 'https://i.pravatar.cc/300', places: 3 },
-		{ id: "u2", name: "Ekaterina Lambeva", image: 'https://i.pravatar.cc/300', places: 2 },
-		{ id: "u3", name: "Stelian Dimitrov", image: 'https://i.pravatar.cc/300', places: 5 }
-	];
+	useEffect(() => {
+		const sendRequest = async () => {
+			setIsLoading(true);
+			try {
+				const response = await fetch('http://localhost:5000/api/users');
+				const responseData = await response.json();
+
+				if (!response.ok) {
+					throw new Error(responseData.message);
+				};
+
+				setUsers(responseData.users);
+				setIsLoading(false);
+			} catch (err) {
+				setIsLoading(false);
+				setError(err.message);
+			};
+		};
+		sendRequest();
+	}, [])
+
+	const errorHandler = () => {
+		setError(null);
+	};
 
 	return (
-		<UsersList items={USERS} />
+		<>
+			<ErrorModal error={error} onClear={errorHandler} />
+			{isLoading && <div className="center"><LoadingSpinner /></div>}
+			{!isLoading && users && <UsersList items={users} />}
+		</>
 	);
 };
 
