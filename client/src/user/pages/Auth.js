df7c39857069bc10b861825cb637d12db299ed29
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-08 11:35:42 +0200

    connecting the front-end with the back-end

--- client/src/user/pages/Auth.js
+++ client/src/user/pages/Auth.js
@@ -21,10 +21,31 @@ const Auth = () => {
 		}
 	}, false)
 
-	const authSubmitHandler = (e) => {
+	const authSubmitHandler = async (e) => {
 		e.preventDefault();
-		console.log(formState.inputs);
-		auth.login();
+		if (isLoginMode) {
+
+
+		} else {
+			try {
+				const response = await fetch("http://localhost:5000/api/users/signup", {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						name: formState.inputs.name.value,
+						email: formState.inputs.email.value,
+						password: formState.inputs.password.value,
+					})
+				})
+				const data = await response.json();
+				console.log(data);
+			} catch (error) {
+				console.log(error);
+			};
+			auth.login();
+		};
 	};
 
 	const switchModeHandler = () => {
--- server/constants.js
+++ server/constants.js
@@ -1,6 +1,6 @@
 
 const PORT = 5000;
-const MONGODB_CONNECTION_STRING = 'mongodb+srv://PetyrDjunov:secretPassword@cubicles.hdzkr.mongodb.net/places?retryWrites=true&w=majority';
+const MONGODB_CONNECTION_STRING = 'mongodb+srv://PetyrDjunov:secretPassword@cubicles.hdzkr.mongodb.net/mern?retryWrites=true&w=majority';
 module.exports = {
 	PORT,
 	MONGODB_CONNECTION_STRING
