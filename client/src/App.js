e8ee7c60dae55a5b3d92748a4a57884f8698bc69
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-09 15:02:52 +0200

    created a custom auth-hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 import { Route, Redirect, Switch, BrowserRouter as Router } from 'react-router-dom';
 
 import NewPlace from './places/pages/NewPlace';
@@ -8,47 +8,10 @@ import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import { AuthContext } from './shared/context/auth-context';
-
-let logoutTimer;
+import { useAuth } from './shared/hooks/auth-hook';
 
 const App = () => {
-  const [token, setToken] = useState(false);
-  const [tokenExpirationDate, setTokenExpirationDate] = useState();
-  const [userId, setUserId] = useState(false);
-
-  const login = useCallback((uid, token, expirationDate) => {
-    setToken(token);
-    setUserId(uid);
-    const tokenExpireDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    console.log(tokenExpireDate);
-    console.log('logging in');
-    setTokenExpirationDate(tokenExpireDate);
-    localStorage.setItem('userData', JSON.stringify({ userId: uid, token, expiration: tokenExpireDate.toISOString() }));
-  }, [])
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setTokenExpirationDate(null);
-    setUserId(null);
-    localStorage.removeItem('userData');
-  }, [])
-
-  useEffect(() => {
-    if (token && tokenExpirationDate) {
-      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    } else {
-      clearTimeout(logoutTimer);
-    };
-  }, [token, logout, tokenExpirationDate]);
-
-  useEffect(() => {
-    const userData = JSON.parse(localStorage.getItem('userData'));
-    if (userData && userData.token && new Date(userData.expiration) > new Date()) {
-      login(userData.userId, userData.token, new Date(userData.expiration))
-    };
-  }, [login]);
-
+  const { token, login, logout, userId } = useAuth();
   let routes;
 
   if (token) {
---
+++ client/src/shared/hooks/auth-hook.js
@@ -0,0 +1,45 @@
+import { useState, useCallback, useEffect } from 'react';
+let logoutTimer;
+
+
+export const useAuth = () => {
+	const [token, setToken] = useState(false);
+	const [tokenExpirationDate, setTokenExpirationDate] = useState();
+	const [userId, setUserId] = useState(false);
+
+	const login = useCallback((uid, token, expirationDate) => {
+		setToken(token);
+		setUserId(uid);
+		const tokenExpireDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+		console.log(tokenExpireDate);
+		console.log('logging in');
+		setTokenExpirationDate(tokenExpireDate);
+		localStorage.setItem('userData', JSON.stringify({ userId: uid, token, expiration: tokenExpireDate.toISOString() }));
+	}, [])
+
+	const logout = useCallback(() => {
+		setToken(null);
+		setTokenExpirationDate(null);
+		setUserId(null);
+		localStorage.removeItem('userData');
+	}, [])
+
+	useEffect(() => {
+		if (token && tokenExpirationDate) {
+			const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+			logoutTimer = setTimeout(logout, remainingTime);
+		} else {
+			clearTimeout(logoutTimer);
+		};
+	}, [token, logout, tokenExpirationDate]);
+
+	useEffect(() => {
+		const userData = JSON.parse(localStorage.getItem('userData'));
+		if (userData && userData.token && new Date(userData.expiration) > new Date()) {
+			login(userData.userId, userData.token, new Date(userData.expiration))
+		};
+	}, [login]);
+
+	return { token, login, logout, userId }
+
+};
