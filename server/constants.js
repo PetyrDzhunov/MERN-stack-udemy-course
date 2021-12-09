5acca7965b5a5576d8d182732f580d20cf366cac
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-09 22:47:35 +0200

    our server/backend is now deployed on heroku

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,5 @@
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const MONGODB_CONNECTION_STRING = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cubicles.hdzkr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 module.exports = {
 	PORT,
