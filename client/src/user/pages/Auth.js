a7a1a1644aa0f84785970f71fea1ad6e7d6c2e1e
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-09 21:43:46 +0200

    create .env and create our variables ready for deployment

--- client/src/user/pages/Auth.js
+++ client/src/user/pages/Auth.js
@@ -31,7 +31,7 @@ const Auth = () => {
 		e.preventDefault();
 		if (isLoginMode) {
 			try {
-				const responseData = await sendRequest("http://localhost:5000/api/users/login",
+				const responseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/users/login`,
 					'POST',
 					JSON.stringify({ email: formState.inputs.email.value, password: formState.inputs.password.value }),
 					{ 'Content-type': 'application/json' }
@@ -46,7 +46,7 @@ const Auth = () => {
 				formData.append('name', formState.inputs.name.value);
 				formData.append('password', formState.inputs.password.value);
 				formData.append('image', formState.inputs.image.value);
-				const responseData = await sendRequest("http://localhost:5000/api/users/signup", 'POST',
+				const responseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/users/signup`, 'POST',
 					formData,
 				);
 				auth.login(responseData.userId, responseData.token);
